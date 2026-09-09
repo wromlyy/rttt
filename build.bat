a83fd291f9b2c1a045c443d2293a03b75d1dc411
wromlyy <wromly@proton.me>
2026-09-09 17:08:45 +0600

    Delete build.bat

--- build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-pip install nuitka pyTelegramBotAPI tabulate PyAutoGUI numpy opencv-python Pillow psutil soundfile sounddevice
-python -m nuitka --mingw64 --onefile --windows-disable-console --follow-imports --remove-output main.pyw
