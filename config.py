5ff7dcd7337b4922704bd48d0e52eb83f700a87f
wromlyy <wromly@proton.me>
2026-09-09 17:10:56 +0600

    Update BOT_TOKEN to a default value
    Set default BOT_TOKEN directly in config.py.

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Можно оставить токен в .env или задать прямо здесь вместо os.getenv(...)
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8866301759:AAGZ6YZJho6u2GjhOvENX3cOlIA0ZzlmqsI").strip()
 MAX_FILE_SIZE = int(os.getenv("MAX_FILE_SIZE", str(5 * 1024 * 1024)))
 DAILY_LIMIT = int(os.getenv("DAILY_LIMIT", "30"))
